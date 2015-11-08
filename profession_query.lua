45be0516c75df82e640f2933da3f23a8334c17ec
Manuel Simon Hirsig <simon.hirsig@gmail.com>
2015-11-08 23:08:44 +0100

    should be working for crafts too now (e.g., enchanting)

--- profession_query.lua
+++ profession_query.lua
@@ -4,13 +4,43 @@ profession_query_skills = {}
 
 function profession_query.on_event()
 	if event == 'TRADE_SKILL_SHOW' then
-		profession_query.update_skills()
+		profession_query.update_trade_skill()
+	elseif event == 'CRAFT_SHOW' then
+		profession_query.update_craft()
 	elseif event == 'CHAT_MSG_WHISPER' then
 		profession_query.respond(arg1, arg2)
 	end
 end
 
-function profession_query.update_skills()
+function profession_query.update_craft()
+	local category
+	for i=1,GetNumCrafts() do
+		local name, _, entry_type = GetCraftInfo(i)
+		if entry_type == 'header' then
+			category = name
+		else
+			local link = GetCraftItemLink(i)
+			if not profession_query_skills[link] then
+				local reagents = {}
+				for j=1,GetCraftNumReagents(i) do
+					tinsert(reagents, {
+						link = GetCraftReagentItemLink(i, j),
+						count = ({ GetCraftReagentInfo(i, j) })[3],
+					})
+				end
+				
+				profession_query_skills[link] = {
+					name = name,
+					category = category,
+					link = link,
+					reagents = reagents,
+				}
+			end
+		end
+	end
+end
+
+function profession_query.update_trade_skill()
 	local category
 	for i=1,GetNumTradeSkills() do
 		local name, entry_type = GetTradeSkillInfo(i)
