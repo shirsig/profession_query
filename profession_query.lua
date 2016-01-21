8685fb11fd7f814011049bb6cd450e6b74f73475
Manuel Simon Hirsig <simon.hirsig@gmail.com>
2016-01-21 11:48:50 +0100

    added slash commands

--- profession_query.lua
+++ profession_query.lua
@@ -97,49 +97,72 @@ function private.update_profession(profession_type)
 	end
 end
 
+function private.response(patterns, limit)
+	local response = {}
+
+	local matches = {}
+	for _, skill in pairs(profession_query_skills) do
+		if private.match(patterns, skill) then
+			tinsert(matches, skill)
+		end
+	end
+		
+	local total_matches = getn(matches)
+	
+	while limit and getn(matches) > limit do
+		tremove(matches, getn(matches))
+	end
+	
+	local pattern = ''
+	for i, part in ipairs(patterns) do
+		if i > 1 then
+			pattern = pattern..' '
+		end
+		pattern = pattern..part
+	end
+	if getn(matches) == 0 then
+		tinsert(response, 'No matches for "'..pattern..'".')
+	elseif getn(matches) < total_matches then
+		tinsert(response, total_matches..' match'..(total_matches > 1 and 'es' or '')..' for "'..pattern..'" ('..(total_matches - limit)..' omitted):')
+	else
+		tinsert(response, total_matches..' match'..(total_matches > 1 and 'es' or '')..' for "'..pattern..'":')
+	end
+	
+	for _, match in ipairs(matches) do
+		local partial_response = match.link..' ='
+		for i, reagent in ipairs(match.reagents) do
+			local reagent_info = string.format(
+				'%s x %i',
+				reagent.link,
+				reagent.quantity
+			)
+			if strlen(partial_response..reagent_info) > 255 then
+				tinsert(response, partial_response)
+				partial_response = '(cont.)'
+			end
+			partial_response = partial_response..' '..reagent_info
+		end
+		tinsert(response, partial_response)
+	end
+	
+	return response
+end
+
 function private.respond(message, sender)
 	local _, _, pattern_string = strfind(message, '^%?(.+)')
 	if pattern_string then
-		local patterns = private.split_on_whitespace(pattern_string)
-
-		local matches = {}
-		for _, skill in pairs(profession_query_skills) do
-			if private.match(patterns, skill) then
-				tinsert(matches, skill)
-			end
-		end
-			
-		local total_matches = getn(matches)
-		
-		while getn(matches) > 3 do
-			tremove(matches, getn(matches))
-		end
-		
-		if getn(matches) == 0 then
-			SendChatMessage('No matches for "'..pattern_string..'"' ,'WHISPER', nil, sender)
-		elseif getn(matches) < total_matches then
-			SendChatMessage(total_matches..' match'..(total_matches > 1 and 'es' or '')..' for "'..pattern_string..'" ('..(total_matches - 3)..' omitted):' ,'WHISPER', nil, sender)
-		else
-			SendChatMessage(total_matches..' match'..(total_matches > 1 and 'es' or '')..' for "'..pattern_string..'":' ,'WHISPER', nil, sender)
-		end
-		
-		for _, match in ipairs(matches) do
-			local partial_response = match.link..' ='
-			for i, reagent in ipairs(match.reagents) do
-				local reagent_info = string.format(
-					'%s x %i',
-					reagent.link,
-					reagent.quantity
-				)
-				if strlen(partial_response..reagent_info) > 255 then
-					SendChatMessage(partial_response ,'WHISPER', nil, sender)
-					partial_response = '(cont.)'
-				end
-				partial_response = partial_response..' '..reagent_info
-			end
-			SendChatMessage(partial_response ,'WHISPER', nil, sender)
+		for _, partial_response in ipairs(private.response(private.split_on_whitespace(pattern_string), 3)) do
+			private.whisper(sender, partial_response)
 		end
 	end
+end
+
+function private.print(message)
+	DEFAULT_CHAT_FRAME:AddMessage(message, 1, 1, 0)
+end
+
+function private.whisper(player, message)
+	SendChatMessage(message,'WHISPER', nil, player)
 end
 
 function private.match(patterns, skill)
@@ -159,4 +182,24 @@ function private.split_on_whitespace(text)
 		tinsert(parts, part)
     end
 	return parts
+end
+
+SLASH_PQ1 = '/pq'
+function SlashCmdList.PQ(parameter)
+	local parameters = private.split_on_whitespace(parameter)
+	if parameters[1] == 'search' and parameters[2] then
+		tremove(parameters, 1)
+		local response = private.response(parameters)
+		for _, partial_response in ipairs(response) do
+			private.print(partial_response)
+		end
+		private.last_printed_response = response
+	elseif parameters[1] == 'send' then
+		local player = parameters[2] or (ChatEdit_GetLastTellTarget(ChatFrameEditBox) ~= '' and ChatEdit_GetLastTellTarget(ChatFrameEditBox))
+		if player then
+			for _, partial_response in ipairs(private.last_printed_response or {}) do
+				private.whisper(player, partial_response)
+			end
+		end
+	end
 end
